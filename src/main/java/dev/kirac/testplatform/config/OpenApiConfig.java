a91c9dd4b765accf4ba9c9a6465fc6a8ee87cfa6
Kıraç Apaydın <kirac.apaydin@yildiztech.com.tr>
2024-10-13 20:40:38 +0300

    fix swagger server url

--- src/main/java/dev/kirac/testplatform/config/OpenApiConfig.java
+++ src/main/java/dev/kirac/testplatform/config/OpenApiConfig.java
@@ -3,15 +3,21 @@ package dev.kirac.testplatform.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import jakarta.servlet.ServletContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI testPlatformOpenAPI() {
+    public OpenAPI testPlatformOpenAPI(ServletContext servletContext) {
+        Server server = new Server().url(servletContext.getContextPath());
         return new OpenAPI()
+                .servers(List.of(server))
                 .info(new Info().title("Test Platform API")
                         .description("Student Test Management System API")
                         .version("v1.0.0")
